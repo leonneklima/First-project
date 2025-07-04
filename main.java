aed44c7e45ecaf2e2c6586d7383bf54eb8d28d8e
LeonnekLima <leonnelimak@gmail.com>
2025-07-03 21:28:12 -0300

    add-

--- main.java
+++ main.java
@@ -9,6 +9,39 @@ public class main {
         boolean estudante = true;
         
 
+        System.out.println("\n--- Informações ---");
+        System.out.println("Nome:" + nome);
+        System.out.println("Idade:" + idade + "anos");
+        System.out.println("Altura:" + altura + "m");
+        System.out.println("É estudante:" + estudante);
+
+
+        int a = 10;
+        int b = 5;
+
+        System.out.println("\n--- Operações ---");
+        System.out.println(a + "+" + b + "=" + (a + b));
+        System.out.println(a + "-" + b + "=" + (a - b));
+        System.out.println(a + "*" + b + "=" + (a * b));
+        System.out.println(a + "/" + b + "=" + (a / b));
+
+
+        if (idade >= 18) {
+            System.out.println("\n" + nome + "é maior de idade!");
+
+        } else {
+            System.out.println("\n" + nome + "é menor de idade");
+        }
+
+        System.out.println("\n--- Contando até 5 ---");
+        for(int i =1; i <= 5; i++) {
+            System.out.println("Número:" + i);
+        }
+
+
+
+
+
 
     }
 }
