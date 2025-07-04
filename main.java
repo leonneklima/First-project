c2b9dc71715b16e33ff937ab5d778c0b19ae9a4b
LeonnekLima <leonnelimak@gmail.com>
2025-07-03 21:08:09 -0300

    add-basic

---
+++ main.java
@@ -0,0 +1,14 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println("Bem-Vindo ao Java!");
+
+        String nome = "Leonardo";
+        int idade = 19;
+        double altura = 1.78;
+        boolean estudante = true;
+        
+
+
+    }
+}
